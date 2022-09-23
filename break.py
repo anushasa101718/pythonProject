ded545ed6133b7e742908a31f59f5045da6deb59
Anusha <anushasa10@my.uri.edu>
2022-09-23 00:08:06 -0400

    Initial commit

---
+++ break.py
@@ -0,0 +1,13 @@
+for i in range(1,11):
+    if i == 5:
+        break
+    else:
+        print(i)
+
+
+list1 = [2,3,4,5,6,7,8,9,12]
+for i in list1:
+    if i%2 != 0:
+        break
+    else:
+        print(i)
