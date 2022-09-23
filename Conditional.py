d9a1c59e7a697226e72613adc4a70df31255c3bb
Anusha <anushasa10@my.uri.edu>
2022-09-22 23:59:28 -0400

    Initial commit

---
+++ Conditional.py
@@ -0,0 +1,14 @@
+# user giving input
+
+x = input('Enter number:')
+print(x)
+
+print(type(x))
+
+# to change the datatype of x to int
+
+x = int(input('Enter number:'))
+print(x)
+print(type(x))
+
+
